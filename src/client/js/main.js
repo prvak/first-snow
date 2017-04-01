f6423ac3b0484042f3c83a10d09809d819e4a452
Michal Mašek <misamasek@gmail.com>
2017-04-01 08:47:41 +0200

    WebSocket skeleton

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,14 +2,34 @@ import React from "react";
 import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+
 import App from "./containers/App.react";
 import reducers from "./reducers";
 
-// Grab the state from a global injected into server-generated HTML
+// Grab the state from a global injected into server-generated HTML.
 const initialState = window.__INITIAL_STATE__;
 
-// Create Redux store with initial state
+// Create Redux store with initial state.
 const store = createStore(reducers, initialState);
+
+// Create websocket connection.
+const ws = new WebSocket("ws://localhost:3000");
+ws.onopen = (event) => {
+  console.log("ws connected to server", event);
+  ws.send(Date.now());
+};
+ws.onmessage = (event) => {
+  console.log("ws message received", event.data);
+  setTimeout(() => {
+    ws.send(Date.now());
+  }, 1000);
+};
+ws.onclose = (event) => {
+  console.log("ws disconnected", event);
+}
+ws.onerror = (event) => {
+  console.log("ws error", event);
+};
 
 render(
   <Provider store={store}>
--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,41 @@
 import Promise from "bluebird";
 import express from "express";
-import Primus from "primus";
 import path from "path";
+import WebSocket from "ws";
+import http from "http";
 
 import logger from "./logger";
 import packageJson from "../../package.json";
-
 
 // Main game server.
 class Server {
   constructor() {
     this.app = express();
+    this.server = http.createServer(this.app);
+    this.wss = new WebSocket.Server({ server: this.server });
 
-    // this.app.use(bodyParser.json());
-    // this.app.use(bodyParser.urlencoded({
-    //   extended: true,
-    // }));
+    this.wss.on("connection", (ws) => {
+      logger.info(`ws client connected: ${ws}`);
+
+      ws.on("message", (message) => {
+        logger.info(`ws message received: ${message}`);
+        ws.send(Date.now());
+      });
+      ws.on("close", (code) => {
+        logger.info(`ws client disconnected: ${code}`);
+      });
+    });
+    // expressWs(this.app);
+
+    // this.app.ws("/ws", (ws, req) => {
+    //   logger.info("connected to client");
+    // });
+
+    // this.app.get("/ws", (req, res) => {
+    //   logger.info("/ws");
+    //   const message = "Not what you wanted.";
+    //   return res.send({ message });
+    // });
 
     this.app.get("/version", (req, res) => {
       logger.info("/version");
@@ -30,24 +50,44 @@ class Server {
     this.config = config;
     const startServer = () => {
       return new Promise((resolve, reject) => {
-        this.server = this.app.listen(this.config.port, this.config.url, (error) => {
+        this.server.listen(this.config.port, this.config.url, (error) => {
           if (error) reject(error);
+          // this.wsClientTest(config);
           resolve();
         }).on("error", (error) => {
           reject(error);
         });
       });
     };
-    const startWebsockets = () => {
-      return Promise.resolve(() => {
-        this.websockets = new Primus(this.server, { transformer: "websockets" });
-      });
-    };
 
     return Promise.resolve()
       .then(startServer)
-      .then(startWebsockets)
       .then(() => { logger.info(`Server listening at 'http://${config.url}:${config.port}'.`); });
+  }
+
+  wsClientTest() {
+    const ws = new WebSocket(`ws://${this.config.url}:${this.config.port}`);
+
+    ws.on("open", () => {
+      logger.info("connected to server");
+      ws.send(Date.now());
+    });
+
+    ws.on("close", () => {
+      logger.info("disconnected");
+    });
+
+    ws.on("message", (data, flags) => {
+      logger.info(`Roundtrip time: ${Date.now() - data} ms`, flags);
+
+      setTimeout(() => {
+        ws.send(Date.now());
+      }, 1000);
+    });
+
+    ws.on("error", (e) => {
+      logger.info("error", e);
+    });
   }
 }
 
