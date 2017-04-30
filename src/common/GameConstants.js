a89859699ffc75c2f71aa2a6e17f47c54594c5ab
Michal Mašek <misamasek@gmail.com>
2017-04-30 19:29:38 +0200

    Add other land cards

--- src/common/GameConstants.js
+++ src/common/GameConstants.js
@@ -40,34 +40,255 @@ export const LandCards = [
     summer: {
       left: {
         resources: [
-          { resource: Resources.FISH },
+          { resource: Resources.BERRY },
+          { resource: Resources.WOOD },
           { resource: Resources.WOOD },
           { resource: Resources.WOOD, item: Items.AXE },
         ],
+        cold: 0,
+        furs: 0,
       },
       right: {
         resources: [
-          { resource: Resources.FISH },
-          { resource: Resources.WOOD },
           { resource: Resources.WOOD, item: Items.AXE },
+          { resource: Resources.WOOD },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.BERRY },
         ],
+        cold: 0,
+        furs: 0,
       },
     },
     winter: {
       left: {
         resources: [
-          { resource: Resources.FISH },
           { resource: Resources.WOOD },
           { resource: Resources.WOOD, item: Items.AXE },
         ],
+        cold: 1,
+        furs: 1,
       },
       right: {
         resources: [
-          { resource: Resources.FISH },
           { resource: Resources.WOOD },
-          { resource: Resources.WOOD, item: Items.AXE },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
         ],
+        cold: 1,
+        furs: 1,
       },
     },
   },
+  {
+    index: 1,
+    summer: {
+      left: {
+        resources: [
+          { resource: Resources.BERRY },
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.GRASS },
+          { resource: Resources.STONE },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.WOOD },
+          { resource: Resources.BIRD, item: Items.BOW },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+    },
+    winter: {
+      left: {
+        resources: [
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.STONE },
+        ],
+        cold: 1,
+        furs: 1,
+      },
+      right: {
+        resources: [
+          { resource: Resources.WOOD },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.BIRD, item: Items.BOW },
+        ],
+        cold: 2,
+        furs: 1,
+      },
+    },
+  },
+  {
+    index: 2,
+    summer: {
+      left: {
+        resources: [
+          { resource: Resources.FISH },
+          { resource: Resources.FISH, item: Items.NET },
+          { resource: Resources.WOOD },
+          { resource: Resources.WOOD },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.GRASS },
+          { resource: Resources.FISH, item: Items.NET },
+          { resource: Resources.FISH, item: Items.NET },
+          { resource: Resources.FISH },
+        ],
+        cold: 1,
+        furs: 0,
+      },
+    },
+    winter: {
+      left: {
+        resources: [
+          { resource: Resources.FISH, item: Items.AXE },
+          { resource: Resources.FISH, item: Items.NET },
+        ],
+        cold: 1,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.GRASS },
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.FISH, item: Items.AXE },
+        ],
+        cold: 2,
+        furs: 1,
+      },
+    },
+  },
+  {
+    index: 3,
+    summer: {
+      left: {
+        resources: [
+          { resource: Resources.BERRY },
+          { resource: Resources.FISH },
+          { resource: Resources.FISH, item: Items.NET },
+          { resource: Resources.GRASS },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.WOOD, item: Items.AXE },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.FISH },
+          { resource: Resources.FISH },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+    },
+    winter: {
+      left: {
+        resources: [
+          { resource: Resources.FISH, item: Items.AXE },
+          { resource: Resources.FISH, item: Items.AXE },
+        ],
+        cold: 2,
+        furs: 1,
+      },
+      right: {
+        resources: [
+          { resource: Resources.WOOD },
+          { resource: Resources.FISH, item: Items.AXE },
+        ],
+        cold: 1,
+        furs: 1,
+      },
+    },
+  },
+  {
+    index: 4,
+    summer: {
+      left: {
+        resources: [
+          { resource: Resources.BERRY },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.GRASS },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.GRASS },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.BERRY },
+        ],
+        cold: 1,
+        furs: 0,
+      },
+    },
+    winter: {
+      left: {
+        resources: [
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+          { resource: Resources.CARIBOU, item: Items.SPEAR },
+        ],
+        cold: 2,
+        furs: 1,
+      },
+      right: {
+        resources: [
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.BIRD, item: Items.BOW },
+        ],
+        cold: 2,
+        furs: 0,
+      },
+    },
+  },
+  {
+    index: 5,
+    summer: {
+      left: {
+        resources: [
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.STONE },
+        ],
+        cold: 1,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.STONE },
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.BERRY },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+    },
+    winter: {
+      left: {
+        resources: [
+          { resource: Resources.BIRD, item: Items.BOW },
+          { resource: Resources.STONE },
+        ],
+        cold: 2,
+        furs: 0,
+      },
+      right: {
+        resources: [
+          { resource: Resources.STONE },
+        ],
+        cold: 0,
+        furs: 0,
+      },
+    },
+  },
+
 ];
